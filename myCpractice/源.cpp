648e1021ddcf2c43da86a8363f306f406f0ddc5c
王宸 苏 <2775253966@qq.com>
2023-10-01 16:12:14 +0800

    23-10-1

--- myCpractice/源.cpp
+++ myCpractice/源.cpp
@@ -1,9 +1,49 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+//int main()
+//{
+//	int line = 0;
+//	printf("加入比特\n");
+//
+//	while (line < 20000)
+//	{
+//		printf("写代码:%d\n", line);
+//		line++;
+//
+//	}
+//
+//	if (line >= 20000)
+//	{
+//		printf("offer\n");
+//	}
+//	else
+//	{
+//		printf("goon\n");
+//	}
+//	return 0;
+//}
+int APP(int x, int y)
+{
+	int z = 0;
+	z = x + y;
+	return z;
+
+}
+
+
+
 int main()
 {
-	printf("hello2");
-	int a;
+	int a, b;
+	scanf("%d %d", &a, &b);
+	int c = APP(a,b);
+	printf("a+b=%d\n", c);
+
+
+
+
+
+
 
 
 
