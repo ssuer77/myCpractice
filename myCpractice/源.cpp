ef48818d766e757c128f1c90223ff7992d8061e5
王宸 苏 <2775253966@qq.com>
2023-10-04 22:41:36 +0800

    23-10-4

--- myCpractice/源.cpp
+++ myCpractice/源.cpp
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 //int main()
 //{
 //	int line = 0;
@@ -58,18 +59,84 @@
 //		printf("%d\n", ch[i]);
 //		i++;
 //	}
-int main()
-{
-	printf("     **\n     **\n************\n************\n    *  *\n    *  *");
-
-
-
-
-	return 0;
-}
-
-
-
+//int main()
+//{
+//	printf("     **\n     **\n************\n************\n    *  *\n    *  *");
+//
+//
+//
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int k = 8;
+//	char e[1] = {'1'};
+//	printf("%d\n", strlen(e)) ;
+//	char d[] = "hello bit";
+//
+//
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//	if (a>=b)
+//	{
+//		printf("%d", a);
+//	}
+//	else
+//	{
+//		printf("%d", b);
+//	}
+//	return 0;
+//}
+//int MAX(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//
+//
+//}
+//
+//
+//int main()
+//{
+//	int a,b;
+//	scanf("%d%d", &a,&b);
+//	int m = MAX(a, b);
+//	printf("%d", m);
+//
+//
+//	return 0;
+//}
+//int DEC(int k)
+//{
+//	if (k < 0)
+//		return 1;
+//	if (k == 0)
+//		return 0;
+//	else
+//		return -1;
+//
+//
+//}
+//int main()
+//{
+//	
+//	int a;
+//	scanf("%d",& a);
+//	int r = DEC(a);
+//	printf("%d\n", r);
+//
+//
+//
+//	return 0;
+//}
 
 
 
