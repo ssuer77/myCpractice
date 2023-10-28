89e9084677a334f4e5a7a40701030abdf7b835e5
王宸 苏 <2775253966@qq.com>
2023-10-28 15:08:19 +0800

    23-10-28

--- myCpractice/源.cpp
+++ myCpractice/源.cpp
@@ -3193,27 +3193,35 @@ using namespace std;
 //	return 0;
 //}
 
-void reverse(char* str)
-{
-	char tmp = *str;
-	int len = strlen(str);
-	*str = *(str + len - 1);
-	*(str + len - 1) = '\0';
-	if (strlen(str + 1)>=2)
-	reverse(str + 1);
-	*(str + len - 1) = tmp;
+//void reverse(char* str)
+//{
+//	char tmp = *str;
+//	int len = strlen(str);
+//	*str = *(str + len - 1);
+//	*(str + len - 1) = '\0';
+//	if (strlen(str + 1)>=2)
+//	reverse(str + 1);
+//	*(str + len - 1) = tmp;
+//
+//}
+//
+//
+//int main()
+//{
+//	char arr[] = "abcdefg";
+//	reverse(arr);
+//	printf("%s\n",arr);
+//
+//	return 0;
+//}
+//
 
-}
+//
 
 
-int main()
-{
-	char arr[] = "abcdefg";
-	reverse(arr);
-	printf("%s\n",arr);
 
-	return 0;
-}
+
+
 
 
 
